7923b88114b2d8030b9d8c231ce8541d88eb2852
Khai Phan Van <phan_van_khai123@yahoo.com>
2021-05-18 05:44:24 +0700

    first commit

---
+++ Article.java
@@ -0,0 +1,44 @@
+package book.java;
+
+/**
+ * 
+ * has info about book
+ * 
+ * 	make getter, setter
+ * 		tính đóng gói	encapsulation
+ * */
+public class Article {
+	// Access Modifier : private public protected (default)
+	// field have to be private
+	// method public
+	private long seq;
+	private String title;
+	private String writerName;
+	
+	public long getSeq() {
+		return seq;
+	}
+	public void setSeq(long seq) {
+		this.seq = seq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriterName() {
+		return writerName;
+	}
+	public void setWriterName(String writerName) {
+		this.writerName = writerName;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	private String content;
+	// alt+shift+s > generate Getter/Setter
+}
